8cead9920a030062bb71fa22b47d8efe738b6f0c
Kazi Muhibul Alam <muhibul@arunima-group.net@NTITAS-PC>
2021-01-22 18:07:41 +0600

    Add project files.

---
+++ ScopoCMS.Web/Controllers/postsController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ScopoCMS.Web.Data;
+using ScopoCMS.Web.Models;
+
+namespace ScopoCMS.Web.Controllers
+{
+    public class postsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public postsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: posts
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.posts.ToListAsync());
+        }
+
+        // GET: posts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.posts
+                .FirstOrDefaultAsync(m => m.postID == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // GET: posts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: posts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(post post, IFormFile image )
+        {
+            if (ModelState.IsValid)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    image.CopyTo(ms);
+                    post.image = ms.ToArray();
+                }
+
+
+                _context.Add(post);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(post);
+        }
+
+        // GET: posts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.posts.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return View(post);
+        }
+
+        // POST: posts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("postID,author,title,publishDate,category,tags,description,imagePath")] post post)
+        {
+            if (id != post.postID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(post);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!postExists(post.postID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(post);
+        }
+
+        // GET: posts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.posts
+                .FirstOrDefaultAsync(m => m.postID == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // POST: posts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var post = await _context.posts.FindAsync(id);
+            _context.posts.Remove(post);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool postExists(int id)
+        {
+            return _context.posts.Any(e => e.postID == id);
+        }
+    }
+}
---
+++ ScopoCMS.Web/Models/post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScopoCMS.Web.Models
+{
+    public class post
+    {
+        public int postID { get; set; }
+        public string author { get; set; }
+        public string title { get; set; }
+
+        public DateTime publishDate { get; set; }
+        public string category { get; set; }
+
+        public string tags { get; set; }
+
+        public string description { get; set; }
+
+        public byte[] image { get; set; }
+
+        
+
+
+    }
+}
